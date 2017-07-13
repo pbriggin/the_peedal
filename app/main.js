1af7e8458c48d97fb2f247d7a9ea22af6873b53c
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-13 10:58:26 -0400

    fix rate detection, no abs

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
 var consoleText = $('#consoleText')[0];
 console.log(consoleText);
+var rates = null;
 var ratesPrev = null;
 window.addEventListener('devicemotion', function(event) {
   // console.log(event.acceleration.x + ' m/s2');
-  var rates = event.rotationRate;
+  rates = event.rotationRate;
   console.log(rates);
 if (ratesPrev != null){
-  if (Math.abs(rates.alpha) > Math.abs(ratesPrev.alpha)) {
-  	consoleText.innerText = 'Speed: ' + Math.abs(rates.alpha);
+  if ((rates.alpha) > (ratesPrev.alpha)) {
+  	consoleText.innerText = 'Speed: ' + Math.abs(Math.round(rates.alpha));
   }
 }
    ratesPrev = rates;
