1adf39597028b98186e5ed9cf0311a1d0da9f7d0
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-13 10:53:05 -0400

    fix ratesPrev not def error

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
 var consoleText = $('#consoleText')[0];
 console.log(consoleText);
-var ratesPrev = {alpha:0,beta:0,gamma:0};
+var ratesPrev = null;
 window.addEventListener('devicemotion', function(event) {
   // console.log(event.acceleration.x + ' m/s2');
   var rates = event.rotationRate;
   console.log(rates);
-
+if (ratesPrev != null){
   if (Math.abs(rates.alpha) > Math.abs(ratesPrev.alpha)) {
   	consoleText.innerText = 'Speed: ' + Math.abs(rates.alpha);
   }
-   var ratesPrev = rates;
+}
+   ratesPrev = rates;
 });
