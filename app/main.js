f75ffb464b62a182092d3eed9a02581eb19df140
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-13 11:21:09 -0400

    add console to all axis

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
-var consoleText = $('#consoleText')[0];
-console.log(consoleText);
+var consoleTextA = $('#consoleTextA')[0];
+var consoleTextB = $('#consoleTextB')[0];
+var consoleTextG = $('#consoleTextG')[0];
+// console.log(consoleTextA);
 var rates = null;
 var ratesPrev = null;
 window.addEventListener('devicemotion', function(event) {
@@ -8,8 +10,16 @@ window.addEventListener('devicemotion', function(event) {
   console.log(rates);
 if (ratesPrev != null){
   if ((rates.alpha) >= (ratesPrev.alpha)) {
-  	consoleText.innerText = 'Speed: ' + Math.abs(Math.round(rates.alpha));
-  	ratesPrev = rates;
+  	consoleTextA.innerText = 'SpeedA: ' + Math.abs(Math.round(rates.alpha));
+  	ratesPrev.alpha = rates.alpha;
+  }
+  if ((rates.beta) >= (ratesPrev.beta)) {
+  	consoleTextB.innerText = 'SpeedB: ' + Math.abs(Math.round(rates.beta));
+  	ratesPrev.beta = rates.beta;
+  }
+  if ((rates.gamma) >= (ratesPrev.gamma)) {
+  	consoleTextG.innerText = 'SpeedG: ' + Math.abs(Math.round(rates.gamma));
+  	ratesPrev.gamma = rates.gamma;
   }
 
 } else {
