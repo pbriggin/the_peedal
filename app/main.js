5f7b7cae8a6c3808065e99217bf278567e7da1ff
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-13 10:45:22 -0400

    add spin detection to lock top speed on screen

--- app/main.js
+++ app/main.js
@@ -1,9 +1,13 @@
 var consoleText = $('#consoleText')[0];
 console.log(consoleText);
-
+var ratesPrev = {alpha:0,beta:0,gamma:0};
 window.addEventListener('devicemotion', function(event) {
   // console.log(event.acceleration.x + ' m/s2');
   var rates = event.rotationRate;
   console.log(rates);
-  consoleText.innerText = 'Speed: ' + rates.alpha; 
+
+  if (Math.abs(rates.alpha) > Math.abs(ratesPrev.alpha)) {
+  	consoleText.innerText = 'Speed: ' + Math.abs(rates.alpha);
+  }
+   var ratesPrev = rates;
 });
