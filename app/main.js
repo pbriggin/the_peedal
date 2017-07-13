bed66e8d3698133a6ea4b8b604d40b485b3a89f9
david-vanderhaar <d.vanderhaarhunter@gmail.com>
2017-07-13 10:30:39 -0400

    add spin listener

--- app/main.js
+++ app/main.js
@@ -1,2 +1,9 @@
-var consoleText = $('#consoleText')
+var consoleText = $('#consoleText')[0];
 console.log(consoleText);
+
+window.addEventListener('devicemotion', function(event) {
+  // console.log(event.acceleration.x + ' m/s2');
+  var rates = event.rotationRate;
+  console.log(rates);
+  consoleText.innerText = 'Speed: ' + rates.alpha; 
+});
